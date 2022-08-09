7d269930a12b3785ac4f2c6246c3501f446dd1bb
Manasi561 <msp561@nyu.edu>
2022-08-09 02:15:32 -0400

    added comments in code file

--- finding_prime_number.py
+++ finding_prime_number.py
@@ -1,17 +1,22 @@
-# Select upper bound
+#Take input from user and store this as upper bound
+#Goal is to dind all the prime numbers below this value
+
+upper_bound = int(input("Enter upper range within which you want prime numbers: "))
+
+# Function to return prime numbers
 
 def prime_num(upper_bound):
     
-    number_range = set(range(2, upper_bound+1,))
+    number_range = set(range(2, upper_bound+1,)) 
     prime_list = []
     
     while number_range:
         
-        prime = number_range.pop()
-        prime_list.append(prime)
-        multiples_of_primes = set(range(prime*2, upper_bound+1, prime))
-        number_range.difference_update(multiples_of_primes)
-    
+        prime = number_range.pop() #Will remove and store the current element
+        prime_list.append(prime)    #append this number to prime number list
+        multiples_of_primes = set(range(prime*2, upper_bound+1, prime)) #remove all multiples of current element
+        number_range.difference_update(multiples_of_primes)  #will update the number_range with only those elements in number_range and not in multiples  
+     
     print(prime_list)
     
     prime_count = len(prime_list)
@@ -20,7 +25,5 @@ def prime_num(upper_bound):
     
     #Summary
     print(f"There are {prime_count} prime numbers below {upper_bound}, the largest being {largest_prime}")
-
-upper_bound = int(input("Enter upper range within which you want prime numbers: "))
 
 prime_num(upper_bound)
